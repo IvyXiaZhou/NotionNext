a9fabe5f96581e9999f8a96a2c1ebb531c650e6b
IvyXiaZhou <212471251+IvyXiaZhou@users.noreply.github.com>
2025-10-16 17:51:21 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -118,10 +118,10 @@ const nextConfig = {
     // 添加这一行来修复 Cloudflare 部署问题
     unoptimized: true,
     // 图片压缩和格式优化
-    formats: ['image/avif', 'image/webp'],
+    //formats: ['image/avif', 'image/webp'],
     // 图片尺寸优化
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+   // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    //imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     // 允许next/image加载的图片 域名
     domains: [
       'gravatar.com',
